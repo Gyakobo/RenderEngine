1a23ae3f54f1f7a6b92e8fd3fb5272f3d657499e
Andrew Gyakobo <andygyakobo@gmail.com>
2022-02-05 00:38:50 -0500

    Fixed Shader problem

--- Mexil/main.cpp
+++ Mexil/main.cpp
@@ -19,12 +19,33 @@ int main(void) {
 		0, 1, 2,
 		2, 3, 0
 	};
-	
-	VertexArray	vao;
-	Buffer*		vbo = new Buffer(vertices, 4*3, 3); 
-	IndexBuffer	ibo(indices, 6);
 
-	vao.addBuffer(vbo, 0);
+
+
+	GLfloat colorsA[] = {
+		1, 0, 1, 1,
+		1, 0, 1, 1,
+		1, 0, 1, 1,
+		1, 0, 1, 1
+	};
+	GLfloat colorsB[] = {
+		0.2f, 0.3f, 0.8f, 1,
+		0.2f, 0.3f, 0.8f, 1,
+		0.2f, 0.3f, 0.8f, 1,
+		0.2f, 0.3f, 0.8f, 1
+	};
+
+
+	
+	VertexArray	sprite1, sprite2; // Vertex Array Object
+	IndexBuffer	ibo(indices, 6); // Index Buffer Object
+
+	sprite1.addBuffer(new Buffer(vertices, 4*3, 3), 0); 
+	sprite1.addBuffer(new Buffer(colorsA, 4*4, 4), 1);
+
+	sprite2.addBuffer(new Buffer(vertices, 4*3, 3), 0); 
+	sprite2.addBuffer(new Buffer(colorsB, 4*4, 4), 1); // The zero refers to the location=0 in "basic.vert"
+ 
 
 
 
@@ -34,11 +55,11 @@ int main(void) {
 	Shader shader("src/shaders/basic.vert", "src/shaders/basic.frag");
 	shader.enable();
 	shader.setUniformMat4("pr_matrix", ortho);
-	shader.setUniformMat4("ml_matrix", mat4::translation(vec3(4, 3, 0)));
+	//shader.setUniformMat4("ml_matrix", mat4::translation(vec3(4, 3, 0)));
 	//shader.setUniformMat4("ml_matrix", mat4::translation(vec3(2, 2, 0)) * mat4::rotation(30.0f, vec3(0, 0, 1)));
 
 
-	//shader.setUniform2f("light_pos", vec2(4.0f, 1.5f));
+	shader.setUniform2f("light_pos", vec2(4.0f, 1.5f));
 	shader.setUniform4f("colour", vec4(0.2f, 0.3f, 0.8f, 1.0f));
 
 
@@ -60,14 +81,27 @@ int main(void) {
 		shader.setUniform2f("light_pos", vec2((float)(x * 16.0f / 960.0f), (float)(9.0f - y * 9.0f / 540.0f)));
 
 
-		vao.bind();
+		sprite1.bind();
 		ibo.bind();
+		shader.setUniformMat4("ml_matrix", mat4::translation(vec3(4, 3, 0)));
 		glDrawElements(GL_TRIANGLES, ibo.getCount(), GL_UNSIGNED_SHORT, 0);
 		ibo.bind();
-		vao.unbind();
+		sprite1.unbind();
+		
+	
+		sprite2.bind();
+		ibo.bind();
+		shader.setUniformMat4("ml_matrix", mat4::translation(vec3(0, 0, 0)));
+		glDrawElements(GL_TRIANGLES, ibo.getCount(), GL_UNSIGNED_SHORT, 0);
+		ibo.bind();
+		sprite2.unbind();
 
 		window.update();
 	}
 
 	return 0;
 }
+
+
+
+
